b5aced550811a00327fe068794a17d00d2af6ae6
p1p3dream <brandon.kirkland1985@gmail.com>
2024-01-25 13:45:01 -0700

    Update ingest_payments.py

--- ingest_payments.py
+++ ingest_payments.py
@@ -4,6 +4,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from datetime import datetime, timedelta
 import pandas as pd
+import requests
 import json
 
 default_args = {
@@ -16,39 +17,54 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-def process_and_upload_to_s3():
-    # Read JSON file
-    with open('/mnt/data/monthly_payments.json', 'r') as file:
-        data = json.load(file)
-    df = pd.DataFrame(data)
-    df = pd.json_normalize(data)
-    
-    # Convert DataFrame to Parquet
-    parquet_buffer = df.to_parquet()
+def get_dummy_json_data(date):
+    # The base URL of the API
+    url = 'https://paymentapp.com/payments'
 
-    # Upload to S3
-    s3_hook = S3Hook(aws_conn_id='your_aws_conn_id')
-    s3_hook.load_bytes(parquet_buffer, 
-                       key='monthly_payments.parquet', 
-                       bucket_name='your_s3_bucket',
-                       replace=True)
+    params = {'date': date.strftime('%Y-%m-%d')}  
+    try:
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def process_and_upload_to_s3():
+    current_date = datetime.today()
+    data = get_dummy_json_data(current_date)
+    if data is not None:
+        df = pd.json_normalize(data)
+        
+        # Convert DataFrame to Parquet
+        parquet_buffer = df.to_parquet()
+
+        # Upload to S3
+        s3_hook = S3Hook(aws_conn_id='your_aws_conn_id')
+        s3_hook.load_bytes(parquet_buffer, 
+                           key='monthly_payments.parquet', 
+                           bucket_name='payments_s3_bucket',
+                           replace=True)
+    else:
+        print("No data received from API.")
 
 def load_into_snowflake():
-    # SQL command to copy data from S3 to Snowflake
     copy_sql = """
     COPY INTO payments
     FROM @your_s3_stage/monthly_payments.parquet
     FILE_FORMAT = (TYPE = 'PARQUET');
     """
-
     return SnowflakeOperator(
         task_id='load_into_snowflake',
-        snowflake_conn_id='your_snowflake_conn_id',
+        snowflake_conn_id='sf_conn_id',
         sql=copy_sql
     )
 
 with DAG('monthly_payments_pipeline', default_args=default_args, schedule_interval=timedelta(days=1)) as dag:
-
     process_upload = PythonOperator(
         task_id='process_and_upload_to_s3',
         python_callable=process_and_upload_to_s3
